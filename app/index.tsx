e7a7f438677b0ef41f23086675b66ec8c327e543
Vineesh M <vineesh1200@gmail.com>
2025-03-27 15:22:42 +0530

    latest

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,12 @@
+import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-2xl font-bold text-primary">Welcome!</Text>
+      <Link  href="/events">Events</Link>
     </View>
   );
 }
