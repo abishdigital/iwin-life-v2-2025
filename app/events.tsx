f4e4f7e69d9d00900ec83220cc8a79a86f4339c2
Vineesh M <vineesh1200@gmail.com>
2025-04-02 16:38:37 +0530

    latest

--- app/events.tsx
+++ app/events.tsx
@@ -1,5 +1,5 @@
 import { ActivityIndicator, FlatList, Image, StyleSheet, Text, Pressable, useWindowDimensions, View } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { images } from '@/constants/images';
 import { icons } from '@/constants/icons';
 import SearchBar from '@/components/searchBar';
@@ -83,53 +83,55 @@ const Events = () => {
     };
 
     return (
-        <View className='flex-1'>
-            <View className='flex-row items-center gap-2 bg-black px-3.5 py-3 w-full'>
-                <Image source={images.logo} style={styles.logo} className='cursor-pointer' />
-                <SearchBar />
-                <Image source={icons.filter} className='cursor-pointer' />
-            </View>
+        <Fragment>
+            <View className='flex-1'>
+                <View className='flex-row items-center gap-2 bg-black px-3.5 py-3 w-full'>
+                    <Image source={images.logo} style={styles.logo} className='cursor-pointer' />
+                    <SearchBar />
+                    <Image source={icons.filter} className='cursor-pointer' />
+                </View>
 
-            <FlatList
-                className="p-4"
-                data={allEvents}
-                keyExtractor={(item) => item.id?.toString() || Math.random().toString()}
-                renderItem={({ item }) => (
-                    <Pressable className="mb-4 border-2 border-gray-500 rounded-lg p-2.5 cursor-pointer" onPress={() => openModal(item)}>
-                        <View className="flex-row items-center gap-3">
+                <FlatList
+                    className="p-4"
+                    data={allEvents}
+                    keyExtractor={(item) => item.id?.toString() || Math.random().toString()}
+                    renderItem={({ item }) => (
+                        <Pressable className="mb-4 border-2 border-gray-500 rounded-lg p-2.5 cursor-pointer" onPress={() => openModal(item)}>
+                            <View className="flex-row items-center gap-3">
 
-                            <Image
-                                source={item.imageURL ? { uri: item.imageURL } : images.notFound}
-                                style={dynamicStyles.mainImage}
-                                defaultSource={images.notFound}
-                                className="rounded-md"
-                            />
+                                <Image
+                                    source={item.imageURL ? { uri: item.imageURL } : images.notFound}
+                                    style={dynamicStyles.mainImage}
+                                    defaultSource={images.notFound}
+                                    className="rounded-md"
+                                />
 
-                            <View className="flex-1">
-                                <Text className="font-viga" style={dynamicStyles.titleText} numberOfLines={1} ellipsizeMode="tail">
-                                    {item.name}
-                                </Text>
-                                <View className='flex-row items-center'>
-                                    <Image defaultSource={images.calendarIcon} style={styles.calendarIcon}></Image>
-                                    <Text className="text-grayCustom font-viga" style={dynamicStyles.dateText} numberOfLines={1} ellipsizeMode="tail">
-                                        {(item.startTime || item.date) && formatDate(item.startTime ?? item.date)} - {(item.endTime || item.date) && formatDate(item.endTime ?? item.date)}
+                                <View className="flex-1">
+                                    <Text className="font-viga" style={dynamicStyles.titleText} numberOfLines={1} ellipsizeMode="tail">
+                                        {item.name}
+                                    </Text>
+                                    <View className='flex-row items-center'>
+                                        <Image defaultSource={images.calendarIcon} style={styles.calendarIcon}></Image>
+                                        <Text className="text-grayCustom font-viga" style={dynamicStyles.dateText} numberOfLines={1} ellipsizeMode="tail">
+                                            {(item.startTime || item.date) && formatDate(item.startTime ?? item.date)} - {(item.endTime || item.date) && formatDate(item.endTime ?? item.date)}
+                                        </Text>
+                                    </View>
+                                    <Text className="text-grayLightCustom font-viga" style={dynamicStyles.descriptionText} numberOfLines={2} ellipsizeMode="tail">
+                                        {item.details}
                                     </Text>
                                 </View>
-                                <Text className="text-grayLightCustom font-viga" style={dynamicStyles.descriptionText} numberOfLines={2} ellipsizeMode="tail">
-                                    {item.details}
-                                </Text>
+
                             </View>
+                        </Pressable>
+                    )}
+                    onEndReached={loadMoreEvents}
+                    onEndReachedThreshold={0.5}
+                    ListFooterComponent={loading ? <ActivityIndicator size="large" color="white" /> : null}
+                />
+            </View>
 
-                        </View>
-                    </Pressable>
-                )}
-                onEndReached={loadMoreEvents}
-                onEndReachedThreshold={0.5}
-                ListFooterComponent={loading ? <ActivityIndicator size="large" color="white" /> : null}
-            />
-
-            <EventModal isVisible={isModalVisible} onClose={closeModal} event={selectedEvent}/>
-        </View>
+            <EventModal isVisible={isModalVisible} onClose={closeModal} event={selectedEvent} />
+        </Fragment>
     );
 };
 
