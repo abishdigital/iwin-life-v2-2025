d211012ca999fc7110e7c58adb46160985db1a1c
primya <primya@iwincompany.com>
2025-04-11 16:12:11 +0530

    latest prod

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,23 @@
-import React from "react";
-import "./globals.css";
-import Events from "./events";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import React from "react";
+// import "./globals.css";
+// import Events from "./events";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+
+// export default function RootLayout() {
+//   return (
+//     <Stack.Navigator initialRouteName="events" screenOptions={{ headerShown: false }}>
+//       <Stack.Screen name="events" component={Events} />
+//     </Stack.Navigator>
+//   )
+// }
+
+
+import { Slot } from 'expo-router';
+import './globals.css';
 
 export default function RootLayout() {
-  return (
-    <Stack.Navigator initialRouteName="events" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="events" component={Events} />
-    </Stack.Navigator>
-  )
+  return <Slot />;
 }
+
