99709e715fe8921bccf7cda62ba68acdb7cc1832
abish.digital <abish.digital@gmail.com>
2025-05-01 01:53:00 +0530

    minor correction

--- services/apiService.tsx
+++ services/apiService.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://wynne.lol/api/',
+    baseURL: 'https://api.iwin.life/',
     headers: {
         Accept: 'application/json',
     },
